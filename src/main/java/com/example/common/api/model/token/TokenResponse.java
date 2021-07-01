0a5724eb839bd6755c4c4b9512e8cdf80466c629
Uladzislau.Barkou <Uladzislau.Barkou@itechart-group.com>
2021-07-01 17:36:00 +0300

    Some refactoring

--- src/main/java/com/example/common/api/model/token/TokenResponse.java
+++ src/main/java/com/example/common/api/model/token/TokenResponse.java
@@ -2,16 +2,18 @@ package com.example.common.api.model.token;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TokenResponse {
     private String token;
     private Long userId;
-    private Date iat;
-    private Date exp;
+    private Date issuedAt;
+    private Date expired;
     private TokenRole role;
 
 }
--- src/main/java/com/example/common/api/model/user/UserDTO.java
+++ src/main/java/com/example/common/api/model/user/UserDTO.java
@@ -1,10 +1,14 @@
 package com.example.common.api.model.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
     private String username;
     private String email;
