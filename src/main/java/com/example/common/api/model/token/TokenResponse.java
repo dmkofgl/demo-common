aea746757720cbd8cb84b536c1608c1e138711f4
Uladzislau.Barkou <Uladzislau.Barkou@itechart-group.com>
2021-07-01 17:25:00 +0300

    Added gradle settings and small refactoring

--- src/main/java/com/example/common/api/model/token/TokenResponse.java
+++ src/main/java/com/example/common/api/model/token/TokenResponse.java
@@ -3,10 +3,8 @@ package com.example.common.api.model.token;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.persistence.Entity;
 import java.util.Date;
 
-@Entity
 @Data
 @AllArgsConstructor
 public class TokenResponse {
